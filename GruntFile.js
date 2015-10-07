ac207a5f2bb78c22fc78d0b96d55ea1613c8acf0
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-10-07 12:44:58 -0700

    initial grunt setup

---
+++ GruntFile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+      // Package
+      pkg: grunt.file.readJSON('package.json'),
+
+      // Uglify
+      uglify: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+                  ' * @author <%= pkg.author %>\n'
+        },
+        build: {
+          src: 'src/<%= pkg.name %>.js',
+          dest: 'build/<%= pkg.name %>.min.js'
+        }
+      }
+      // Sass
+      sass: {
+          dev: {
+            options: {
+              style: 'expanded',
+              banner: '<%= tag.banner %>',
+              compass: true
+            },
+            files: {
+              '<%= project.assets %>/css/style.css': '<%= project.css %>'
+            }
+          },
+          dist: {
+            options: {
+              style: 'compressed',
+              compass: true
+            },
+            files: {
+              '<%= project.assets %>/css/style.css': '<%= project.css %>'
+            }
+          }
+        },
+        // Watch
+        watch: {
+          sass: {
+            files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
+            tasks: ['sass:dev']
+          }
+        }
+    });
+};
+
+// Load Plugins (matchdep loads all others)
+require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+// Default task
+grunt.registerTask('default', [
+  'sass:dev',
+  'watch'
+]);
+// Deploy task
+grunt.registerTask('deploy', [
+  'sass:dist',
+  'uglify:build'
+]);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # akhub
+
+Basic entry page for various subdomains of www.arielkaplan.com
+
+## grunt
+Packages used:
+- Uglify
