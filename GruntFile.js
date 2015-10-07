93f9fc5967e94747db08ac7ea361f8ebdf76356d
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-10-07 16:11:42 -0700

    Fix GruntFile bugs; fill in file structure

--- GruntFile.js
+++ GruntFile.js
@@ -1,62 +1,70 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-      // Package
-      pkg: grunt.file.readJSON('package.json'),
+        // Package
+        pkg: grunt.file.readJSON('package.json'),
 
-      // Uglify
-      uglify: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' +
-                  ' * @author <%= pkg.author %>\n'
+        // Uglify
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+                ' * @author <%= pkg.author %>\n'
+            },
+            build: {
+                src: 'js/main.js',
+                dest: 'deploy/js/main.min.js'
+            }
         },
-        build: {
-          src: 'src/<%= pkg.name %>.js',
-          dest: 'build/<%= pkg.name %>.min.js'
-        }
-      }
-      // Sass
-      sass: {
-          dev: {
-            options: {
-              style: 'expanded',
-              banner: '<%= tag.banner %>',
-              compass: true
+        // Sass
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    // banner: '<%= tag.banner %>',
+                    // compass: true
+                },
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                }
             },
-            files: {
-              '<%= project.assets %>/css/style.css': '<%= project.css %>'
+            dist: {
+                options: {
+                    style: 'compressed',
+                    // compass: true
+                },
+                files: {
+                    'deploy/css/style.css': 'scss/style.scss'
+                }
             }
-          },
-          dist: {
-            options: {
-              style: 'compressed',
-              compass: true
-            },
-            files: {
-              'css/style.css': 'scss/style.scss %>'
-            }
-          }
         },
         // Watch
         watch: {
-          sass: {
-            files: 'scss/{,*/}*.{scss,sass}',
-            tasks: ['sass:dev']
-          }
+            grunt: {
+                files: ['Gruntfile.js']
+            },
+            sass: {
+                files: 'scss/{,*/}*.{scss,sass}',
+                tasks: ['sass:dev']
+            }
         }
     });
+
+    // Load Plugins (matchdep loads all others)
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // grunt.loadNpmTasks('grunt-contrib-sass');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('matchdep');
+
+    // Default task
+    grunt.registerTask('default', [
+        'sass:dev',
+        'watch'
+    ]);
+    // Deploy task
+    grunt.registerTask('deploy', [
+        'sass:dist',
+        'uglify:build'
+    ]);
+
 };
-
-// Load Plugins (matchdep loads all others)
-require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-// Default task
-grunt.registerTask('default', [
-  'sass:dev',
-  'watch'
-]);
-// Deploy task
-grunt.registerTask('deploy', [
-  'sass:dist',
-  'uglify:build'
-]);
